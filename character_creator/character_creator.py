5d142aff0e833bd930277be783e9b2aabd817aa9
Adam Bigelow <53096373+abigelow-io@users.noreply.github.com>
2019-11-26 05:54:55 -0500

    updates to assign function. NOT FUNCTIONAL

--- character_creator/character_creator.py
+++ character_creator/character_creator.py
@@ -52,12 +52,71 @@ class Character:
 
     # Assignment function for manual stat assignment
 
+
     def assign(self):
-        return None
-        # Finish later.
         
+        #Assign stats RANDOM
+        
+        self.roll1 = stat_roll()
+        self.roll2 = stat_roll()
+        self.roll3 = stat_roll()
+        self.roll4 = stat_roll()
+        self.roll5 = stat_roll()
+        self.roll6 = stat_roll()
+        
+        stats_list = [self.roll1, self.roll2, self.roll3, self.roll4, self.roll5, self.roll6]
 
 
+        stats = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
+
+        strength = self.Strength
+        dexterity = self.Dexterity
+        consitution = self.Constituion
+        intelligence = self.Intelligence
+        wisdom = self.Wisdom
+        charisma = self.Charisma
+
+        stat_vars = [strength, dexterity, consitution, intelligence, wisdom, charisma]
+
+        remaining_totals = 6
+
+        print("Rolls:")
+        print("Roll 1: "+str(self.roll1))
+        print("Roll 2: "+str(self.roll2))
+        print("Roll 3: "+str(self.roll3))
+        print("Roll 4: "+str(self.roll4))
+        print("Roll 5: "+str(self.roll5))
+        print("Roll 6: "+str(self.roll6))
+
+        # While stats_list is longer than 0 entries, continue
+
+        while remaining_totals > 0 and True:
+            print("Your remaining totals are "+ str(stats_list))
+            total_select = input("Please choose a total to be assigned to a stat:   ")
+
+            if total_select in stats_list:
+                chosen_total_index = stats_list.index(total_select)
+                stats_list.remove(chosen_total_index)
+                remaining_totals -= 1
+                stat_choice = input("Please choose a stat for assignemnt")
+                stat_choice.lower()
+                success = 0
+
+                while success == 0:
+                    if i in stats != stat_choice:
+                        print("Please choose a valid stat by typing the whole word")
+                        success = 0
+                    else:
+                        stat_choice_index = stats.index(stat_choice)
+                        stat_choice = stat_vars[stat_choice_index]
+                        stat_choice = total_select
+                        print("You have assigned "+total_select+" as the value for "+stat_choice+".")
+            else:
+                print("Please choose a total from the provided list.")
+                return True
+                
+
+        print("All stats assigned")                
     # Roll Stats
 
     def roll_stats(self):
@@ -79,6 +138,8 @@ class Character:
         elif str(self.std_set) == "Y" or str(self.std_set) == "Yes" and self.assignment_method == "No":
             stats_list = [15, 14, 13, 12, 10, 8]
             # run outer scope assignment func
+        elif str(self.std_set) == "N" or str(self.std_set) == "No" and self.assignment_method == "No":
+            assign(self)
         else:
             print("Did not receive valid response regarding stat creation. Proceeding with standard stats.")
             self.Strength = 15
@@ -96,6 +157,11 @@ class Character:
         print("Intelligence = "+str(self.Intelligence))
         print("Wisdom = "+str(self.Wisdom))
         print("Charisma = "+str(self.Charisma))
+
+
+adam = Character("Adam", "pig")
+adam.assign()
+adam.declare()
 
 
 
