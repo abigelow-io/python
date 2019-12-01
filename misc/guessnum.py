d8ffafe0fc1d0ef6070e055fc4df635fde756e53
Adam Bigelow <53096373+abigelow-io@users.noreply.github.com>
2019-12-01 16:06:19 -0500

    Update to valid responses

--- misc/guessnum.py
+++ misc/guessnum.py
@@ -3,7 +3,7 @@ print("I am thinking of a number between 1 and 20. What is it?")
 print("You have 4 tries.")
 correct_answer = random.randint(1, 20)
 guess = 0
-valid_responses = list(range(1,20))
+valid_responses = list(range(1,21))
 tries = 0
 
 
