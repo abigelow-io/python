9dfc775ab7d2e922c5ab10b222683451a233888c
Adam Bigelow <ssdshw@gmail.com>
2019-12-03 05:29:55 -0500

    added string validation

--- misc/testpush.py
+++ misc/testpush.py
@@ -4,3 +4,11 @@ if answer == 4:
     print("Correct")
 else:
     print("Incorrect")
+
+print("Please type any string")
+answer2 = input()
+
+try:
+    answer2_str = str(answer2)
+except ValueError:
+    print("You didn't provide a string")
