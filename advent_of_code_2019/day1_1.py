ae4f92c8aba690238714e940849614e08d735298
Adam Bigelow <53096373+abigelow-io@users.noreply.github.com>
2019-12-09 04:21:32 -0500

    Solved for Day 1 part 2

--- advent_of_code_2019/day1_1.py
+++ advent_of_code_2019/day1_1.py
@@ -9,10 +9,29 @@ data_ints = [int(i) for i in data_list]
 
 def fuel_solve(mass):
     return (mass//3)-2
-    
+
 fuel_amounts = [fuel_solve(i) for i in data_ints]
 
+#The below line returned the answer for Part 1
 print(sum(fuel_amounts))
 
+# BEGIN PART 2
 
+def fuel_two(mass):
+    temp_total = 0
+    target = mass
+    while True:
+        fuel_needed = fuel_solve(target)
+        print(fuel_needed)
+        if fuel_needed > 0:
+            temp_total += fuel_needed
+        target = fuel_needed
+        if fuel_needed > 0:
+            continue
+        else:
+            return temp_total
+            return False
+
+new_fuels = [fuel_two(i) for i in data_ints]
+print(sum(new_fuels))
 
